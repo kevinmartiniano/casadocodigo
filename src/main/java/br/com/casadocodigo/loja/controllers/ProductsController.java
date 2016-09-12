fa819db28bfbc7993707b1f176f397616ad6f54c
kevinmartiniano <kevin.martiniano123@gmail.com>
2016-09-11 22:52:18 -0300

    Casa do Código, com erro 500 no JPA

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -0,0 +1,35 @@
+package br.com.casadocodigo.loja.controllers;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.Product;
+
+@Controller
+@Transactional
+public class ProductsController {
+
+	@Autowired
+	private ProductDAO productDAO;
+
+	public String save(Product product) {
+		productDAO.save(product);
+		return "products/ok";
+	}
+/*
+	@RequestMapping("/produtos")
+	public String save(Product produto) {
+		System.out.println("Cadastrando produto" + produto);
+		return "products/ok";
+	}
+*/
+	@RequestMapping("produtos/form")
+	public String form() {
+		return "produtos/form";
+	}
+
+}
